144b163a405bc47fbea8fe8d2a40a0d8cd236df3
Sujal Sinha <sujalsinha2243@gmail.com>
2023-07-28 00:53:36 +0530

    implement lazy loading in artist show page

--- public/javascripts/artistShow.js
+++ public/javascripts/artistShow.js
@@ -1,9 +1,10 @@
 const audios = document.querySelectorAll("audio");
 
-function pauseOtherAudios(a) {
+function pauseOtherAudios(currentAudio) {
 	for (const audio of audios) {
-		if (audio !== a) {
-			audio.parentElement.parentElement.classList.remove("active");
+		if (audio !== currentAudio) {
+			const listItem = audio.closest(".oneAudio");
+			listItem.classList.remove("active");
 			audio.nextElementSibling.innerHTML = "";
 			audio.pause();
 			audio.currentTime = 0;
@@ -12,11 +13,6 @@ function pauseOtherAudios(a) {
 }
 
 for (const audio of audios) {
-	audio.addEventListener("play", () => {
-		audio.parentElement.parentElement.classList.add("active");
-		pauseOtherAudios(audio);
-		audio.nextElementSibling.innerHTML = "play_arrow";
-	});
 	audio.addEventListener("timeupdate", () => {
 		if (audio.currentTime === audio.duration) {
 			audio.pause();
@@ -27,7 +23,14 @@ for (const audio of audios) {
 
 function togglePlay(audio) {
 	if (audio.paused) {
+		if (!audio.src) {
+			audio.src = audio.getAttribute("data-src");
+		}
+
+		audio.parentElement.parentElement.classList.add("active");
 		audio.nextElementSibling.innerHTML = "play_arrow";
+
+		pauseOtherAudios(audio);
 		audio.play();
 	} else {
 		audio.nextElementSibling.innerHTML = "";
