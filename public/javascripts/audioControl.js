3b77add042e39478f6606162cfac8d02612a939f
Sujal Sinha <sujalsinha2243@gmail.com>
2023-07-28 00:32:52 +0530

    implement lazy loading and remove duplication to improve performance

--- public/javascripts/audioControl.js
+++ public/javascripts/audioControl.js
@@ -1,42 +1,11 @@
 const audios = document.querySelectorAll("audio");
 
-function pauseOtherAudios(a) {
-	for (const audio of audios) {
-		if (audio !== a) {
-			audio.parentElement.parentElement.classList.remove("active");
-			audio.nextElementSibling.innerHTML = "";
-			audio.pause();
-			audio.currentTime = 0;
-			document.querySelector("#player-play").innerHTML = "play_arrow";
-		}
-	}
+function initProgressBar(audio) {
+	let progressbar = document.getElementById("player-progress");
+	progressbar.value = audio.currentTime / audio.duration;
 }
 
 for (const audio of audios) {
-	audio.addEventListener("play", () => {
-		audio.parentElement.parentElement.classList.add("active");
-		pauseOtherAudios(audio);
-		audio.nextElementSibling.innerHTML = "equalizer";
-		document.querySelector("#player-play").innerHTML = "pause";
-		for (song of songs) {
-			if (
-				song.title ===
-				audio.parentElement.parentElement.children[1].innerHTML
-			) {
-				document
-					.querySelector("#player-controls")
-					.removeAttribute("hidden");
-				document.querySelector("progress").removeAttribute("hidden");
-				const image = document.querySelector("#player-image");
-				image.src = song.artist.image;
-				document.querySelector("#player-song").innerHTML = song.title;
-				document.querySelector("#player-artist").innerHTML =
-					song.artist.name;
-				document.querySelector("#player-lang").innerHTML =
-					song.language;
-			}
-		}
-	});
 	audio.addEventListener("timeupdate", () => {
 		initProgressBar(audio);
 		if (audio.currentTime === audio.duration) {
@@ -51,43 +20,70 @@ for (const audio of audios) {
 	});
 }
 
+function updatePlayerDetails(songTitle) {
+	const currentSong = songs.find((song) => song.title === songTitle);
+
+	if (currentSong) {
+		const image = document.querySelector("#player-image");
+		image.src = currentSong.artist.image;
+		document.querySelector("#player-controls").removeAttribute("hidden");
+		document.querySelector("progress").removeAttribute("hidden");
+		document.querySelector("#player-song").innerHTML = currentSong.title;
+		document.querySelector("#player-artist").innerHTML =
+			currentSong.artist.name;
+		document.querySelector("#player-lang").innerHTML = currentSong.language;
+	}
+}
+
 function togglePlay(audio) {
 	if (audio.paused) {
+		if (!audio.src) {
+			audio.src = audio.getAttribute("data-src");
+		}
+
+		audio.parentElement.parentElement.classList.add("active");
+		audio.nextElementSibling.innerHTML = "equalizer";
 		document.querySelector("#player-play").innerHTML = "pause";
+
+		pauseOtherAudios(audio);
+
+		const songTitle =
+			audio.parentElement.parentElement.children[1].innerHTML;
+		updatePlayerDetails(songTitle);
 		audio.play();
 	} else {
 		audio.nextElementSibling.innerHTML = "";
 		document.querySelector("#player-play").innerHTML = "play_arrow";
+
 		audio.pause();
 	}
 }
 
-const skipNext = (currentTrack) => {
-	const currentAudio = currentTrack.children[3].children[0];
+function skipNext(currentTrack) {
 	const nextTrack = currentTrack.parentElement.nextElementSibling.children[0];
-	if (currentTrack.parentElement.nextElementSibling) {
-		currentAudio.pause();
-		currentAudio.currentTime = 0;
-		currentTrack.classList.remove("active");
-		nextTrack.classList.add("active");
-		nextTrack.children[3].children[0].play();
+	const nextAudio = nextTrack.querySelector("audio");
+	if (nextTrack) {
+		togglePlay(nextAudio);
 	}
-};
+}
 
-const skipPrev = (currentTrack) => {
-	const currentAudio = currentTrack.children[3].children[0];
+function skipPrev(currentTrack) {
 	const prevTrack =
 		currentTrack.parentElement.previousElementSibling.children[0];
-	if (currentTrack.parentElement.previousElementSibling) {
-		currentAudio.pause();
-		currentAudio.currentTime = 0;
-		currentTrack.classList.remove("active");
-		prevTrack.classList.add("active");
-		prevTrack.children[3].children[0].play();
+	const prevAudio = prevTrack.querySelector("audio");
+	if (prevTrack) {
+		togglePlay(prevAudio);
 	}
-};
+}
 
-function initProgressBar(audio) {
-	let progressbar = document.getElementById("player-progress");
-	progressbar.value = audio.currentTime / audio.duration;
+function pauseOtherAudios(currentAudio) {
+	for (const audio of audios) {
+		if (audio !== currentAudio) {
+			const listItem = audio.closest(".oneAudio");
+			listItem.classList.remove("active");
+			audio.nextElementSibling.innerHTML = "";
+			audio.pause();
+			audio.currentTime = 0;
+		}
+	}
 }
