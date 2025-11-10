b50032af05af88300d4347fdf5941444e8bc0c6b
Sujal Sinha <sujalsinha2243@gmail.com>
2025-11-10 23:50:54 +0530

    upgrade node from v18 to v20 and fix breaking changes, vulnerabilities

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ const dbUrl = process.env.DB_URL;
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 
-mongoose.connect(dbUrl, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
